3e2d8dda260589f8d47962fc2c51396dccaccf4d
Adam Knezevic <adam_knezi@outlook.com>
2016-07-25 17:53:18 +1000

    Buttons work
    Wew, buttons are working

--- MarketViewer/Form1.vb
+++ MarketViewer/Form1.vb
@@ -1,3 +1,20 @@
-﻿Public Class frmMain
+﻿'Project - MarketViewer
+'Author - Adam "domacikolaci" Knezevic
+'Last modified - Adam Knezevic 25/07/2016
 
+Public Class frmMain
+    Private Sub btnStocks_Click(sender As Object, e As EventArgs) Handles btnStocks.Click
+        'Loads frmStock
+        frmStock.Show()
+    End Sub
+
+    Private Sub btnCrypto_Click(sender As Object, e As EventArgs) Handles btnCrypto.Click
+        'Load frmCrypto
+        frmCrypto.Show()
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        'Load frmAbout
+        frmAbout.Show()
+    End Sub
 End Class
---
+++ MarketViewer/crypto.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmCrypto
+
+End Class
